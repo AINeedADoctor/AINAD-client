176f8897142db08a3b4fafbb968b3ff172476c99
Guillem96 <guillem.orellana@gmail.com>
2018-10-21 00:22:10 +0200

    Bot working

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,20 +1,24 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { ChatComponent } from './components/chat/chat.component';
 import { MessageComponent } from './components/chat/message/message.component';
+import { HttpClientModule } from '@angular/common/http';
 
 @NgModule({
   declarations: [
     AppComponent,
     ChatComponent,
-    MessageComponent
+    MessageComponent,
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/components/chat/chat.component.ts
+++ src/app/components/chat/chat.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { DialogflowService } from 'src/app/services/dialogflow.service';
 
 @Component({
   selector: 'app-chat',
@@ -6,10 +7,25 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./chat.component.css']
 })
 export class ChatComponent implements OnInit {
-
-  constructor() { }
+  
+  public message: string;
+  public messages: Array<{
+    content: string,
+    bot: boolean
+  }> = [];
+  constructor(private dialogFlow: DialogflowService) { }
 
   ngOnInit() {
   }
 
+  public sendMessageToClank() {
+    this.dialogFlow.sendToClank(this.message).subscribe(
+      res => {
+        this.messages.push({ bot: false, content: this.message });
+        this.messages.push({ bot: true, content: res});
+        this.message = "";
+      },
+      err => { alert(err); }
+    );
+  }
 }
