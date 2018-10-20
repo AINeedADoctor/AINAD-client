847ebef3e931bcabf050de7f8eb1cafdc908b896
Guillem96 <guillem.orellana@gmail.com>
2018-10-20 22:07:02 +0200

    Starting with chat

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,9 +1,15 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MzInputModule } from 'ngx-materialize'
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { ChatComponent } from './components/chat/chat.component';
+
+import { MzCardModule } from 'ngx-materialize'
+import { MzButtonModule } from 'ngx-materialize'
+
 
 @NgModule({
   declarations: [
@@ -12,7 +18,11 @@ import { ChatComponent } from './components/chat/chat.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MzCardModule,
+    MzInputModule,
+    MzButtonModule
   ],
   providers: [],
   bootstrap: [AppComponent]
