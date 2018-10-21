bdf3ffe0b5b970dbd041315565d68ada624721d1
Guillem96 <guillem.orellana@gmail.com>
2018-10-21 05:13:09 +0200

    Animation to messages

--- src/app/components/chat/chat.component.ts
+++ src/app/components/chat/chat.component.ts
@@ -1,22 +1,34 @@
-import { Component, OnInit, HostListener } from '@angular/core';
+import { Component, OnInit, HostListener, ViewChild, ElementRef } from '@angular/core';
 import { DialogflowService } from 'src/app/services/dialogflow.service';
 import { GooglePlacesService } from 'src/app/services/google-places.service';
+import { trigger, transition, style, state, animate } from '@angular/animations';
+import { query } from '@angular/core/src/render3';
 
 
 @Component({
   selector: 'app-chat',
   templateUrl: './chat.component.html',
-  styleUrls: ['./chat.component.css']
+  styleUrls: ['./chat.component.css'],
+  animations: [
+    trigger('Fading', [
+      state('void', style({ opacity: 0 })),
+      state('*', style({ opacity: 1 })),
+      transition(':enter', animate('1000ms ease-out')),
+    ])
+  ]
 })
 export class ChatComponent implements OnInit {
-  
+
   public message: string;
   public messages: Array<{
     content: string,
     bot: boolean
   }> = [];
+
+  @ViewChild('scrollMe') private messagesContainer: ElementRef;
+
   constructor(private dialogFlow: DialogflowService,
-              private googlePlaces: GooglePlacesService) { }
+    private googlePlaces: GooglePlacesService) { }
 
   @HostListener('document:keypress', ['$event'])
   handleKeyboardEvent(event: KeyboardEvent) {
@@ -25,13 +37,22 @@ export class ChatComponent implements OnInit {
     }
   }
 
-  ngOnInit() {
+  ngOnInit() { 
+    this.scrollToBottom();
+  }
+
+  ngAfterViewChecked() {
+    this.scrollToBottom();
+  }
+
+  scrollToBottom(): void {
+    this.messagesContainer.nativeElement.scrollTop = this.messagesContainer.nativeElement.scrollHeight;
   }
 
   public sendMessageToClank() {
     if (this.message === "")
       return;
-      
+
     if (this.message.includes("hospital")) {
       this.messages.push({ bot: false, content: this.message });
       this.mockClank("hospital");
@@ -47,7 +68,7 @@ export class ChatComponent implements OnInit {
     this.dialogFlow.sendToClank(this.message).subscribe(
       res => {
         this.messages.push({ bot: false, content: this.message });
-        this.messages.push({ bot: true, content: res});
+        setTimeout(() => this.messages.push({ bot: true, content: res }), 1000);
         this.message = "";
       },
       err => { alert(err); }
@@ -58,6 +79,7 @@ export class ChatComponent implements OnInit {
     this.googlePlaces.getPlace(body).subscribe(
       res => {
         this.messages.push({ bot: true, content: "<b>" + res.name + '</b><br><a href="' + res.google_link + '"><i class="fas fa-map-marker-alt"></i> Google maps</a>' });
+        this.message = "";
       },
       err => {
         console.log(err);
